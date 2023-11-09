b6960a35dc55b1a9871fc13f00637948cd5f4815
Anjula Perera <anjulaperera98@gmail.com>
2023-11-09 18:08:37 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+/// <reference path="global.d.ts" />
+const app = require("express")();
+import { Request, Response } from "express";
+
+const cors = require("cors");
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+app.use(cors());
+
+var port = process.env.PORT || 6000;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+
+app.use(function (req: Request, res: Response) {
+  res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
+app.listen(port, () => {
+  console.log(`API server started on: ${port}`);
+});
